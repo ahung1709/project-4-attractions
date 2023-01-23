83322c22c03c3714b4b284901b1fc65b803c463e
Aaron Hung <ahungwork@yahoo.ca>
2023-01-23 09:06:41 -0800

    add user model, implement create controller function

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,25 +1,42 @@
+const jwt = require('jsonwebtoken')
+const User = require("../../models/user")
 
 module.exports = {
     create, 
 }
 
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+    return jwt.sign(
+      // data payload
+      { user },
+      process.env.SECRET,
+      { expiresIn: '24h' }
+    );
+  }
+
 async function create(req, res) {
-    // try {
-    //     // make a new user in a mongoose database
-    //     const user = await User.create(req.body)
-    //     // token will be a string
-    //     const token = createJWT(user)
-    //     res.status(200).json(token)
+    try {
+        // make a new user in a mongoose database
+        const user = await User.create(req.body)
+        // token will be a string
+        const token = createJWT(user)
+        // Yes, we can use res.json to send back just a string
+        // The client code needs to take this into consideration
+        res.status(200).json(token)
     //     console.log(user)
     //     // create JWT
     //     // response with the JWT
-    // } catch(err) {
-    //     res.status(400).json(err)
-    // }
-    res.json({
-        user: { 
-            name: req.body.name, 
-            email: req.body.email
-        }
-    })
+    } catch(err) {
+        // Client will check for non-2xx status code 
+        // 400 = Bad Request
+        res.status(400).json(err)
+    }
+    // res.json({
+    //     user: { 
+    //         name: req.body.name, 
+    //         email: req.body.email
+    //     }
+    // })
 }
